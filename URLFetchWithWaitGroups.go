a1b07c4a7830028d5943880b87e0d8d081184b3c
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-12 14:45:02 +0530

    distributed URL fetch

---
+++ URLFetchWithWaitGroups.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var urls = []string{
+	"https://www.google.com",
+	"https://www.somewhere.com",
+	"https://www.yahoo.com",
+	"https://www.someplace.com",
+}
+
+func fetchUrl(url string, wg *sync.WaitGroup){
+	defer wg.Done()
+    resp, err := http.Get(url)
+    if err!= nil {
+        fmt.Println(err)
+       
+    }
+    
+    fmt.Println(resp.Status)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request){
+	var wg sync.WaitGroup
+	fmt.Println("Home endpoint")
+	wg.Add(len(urls))
+	for _, url := range urls {
+        go fetchUrl(url, &wg)
+    }
+	wg.Wait()
+	fmt.Fprintf(w, "all responses received successfully")
+
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homeHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+    handleRequests()
+}
