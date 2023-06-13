a468df04269a53420b180d3f1695c3e6ee4504d4
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-13 09:29:28 +0530

    parallel Generator

---
+++ DesignPatterns/Generator/FibonacciParallel.go
@@ -0,0 +1,28 @@
+package main
+
+import ("fmt"
+		"sync")
+
+var wg sync.WaitGroup
+
+func main() {
+	for val := range producer() {
+		fmt.Println(val)
+			}
+		}
+
+func producer() chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		defer wg.Wait()
+		for i := 0; i < 100; i+=5 {
+			wg.Add(i)
+			go func(i int){
+				defer wg.Done()
+                out <- i
+			}(i)
+		}
+	}()
+	return out
+}
